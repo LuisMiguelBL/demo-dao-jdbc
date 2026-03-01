ab5ccb12e7164fd26e3d2747f40742d5f45d7dda
Luís Miguel Belfort <vilanovamiguel1@gmail.com>
2026-03-01 14:05:56 -0300

    Create entidy sellerDaoJDBC and DaoFactory

---
+++ src/model.dao.impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.Seller;
+import model.SellerDao;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller Seller) {
+
+    }
+
+    @Override
+    public void update(Seller Seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
